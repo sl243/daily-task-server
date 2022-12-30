124e3d36c11430da6fc10dec33f3774e0434f23e
sl243 <slmiucse@gmail.com>
2022-12-30 16:26:29 +0600

    daily task post method added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config()
 
@@ -9,6 +10,43 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2elctou.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const taskCollection = client.db('dailyTask').collection('task')
+        const addtasksCollection = client.db('dailyTask').collection('addtasks')
+      
+        //  get task
+        app.get('/alltask', async(req, res) => {
+          const query = {};
+          const alltask = await taskCollection.find(query).toArray();
+          res.send(alltask)
+        })
+        
+        // task added in databse
+        app.post('/tasks', async(req, res) => {
+            const dailyTask = req.body;
+            console.log(dailyTask)
+            const result = await taskCollection.insertOne(dailyTask);
+            res.send(result)
+        })
+
+        // add task
+        app.post('/addtasks', async(req, res) => {
+          const addtask = req.body;
+          const result = await addtasksCollection.insertOne(addtask);
+          res.send(result)
+        })
+    }
+    finally{
+
+    }
+    run().catch(console.dir)
+}
+
 
 app.get('/', (req, res) => {
   res.send('Daily Task')
